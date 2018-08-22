449194cee47b6dff160f57ec88259850fc1e0b8b
Nikhil Arora <narora200@gmail.com>
2018-08-22 08:45:27 +0530

    [Feat]: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Everyday Education</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
