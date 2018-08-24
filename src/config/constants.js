f670f50598fb7317ca373ff976ae7f46bdbffe1a
Nikhil Arora <narora200@gmail.com>
2018-08-24 15:31:12 +0530

    [Feat]: NewLearning component created

---
+++ src/config/constants.js
@@ -0,0 +1,12 @@
+const URLS = {
+  BACKEND_URL: 'http://localhost:8000',
+  FRONTEND_URL: 'http://localhost:3000',
+};
+
+const env = process.env.NODE_ENV;
+if ((env === 'production') || (env === 'staging')) {
+  URLS.BACKEND_URL = 'https://everyday-education.now.sh';
+  URLS.FRONTEND_URL = 'https://everyday-education.netlify.com';
+}
+
+export { URLS };
